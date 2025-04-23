7c710c12900687c877284c7f5b22f2afe48f412b
akim107 <kahnhilary30@gmail.com>
2025-04-22 21:08:16 -0400

    17th commit
    [4/22, 9:08 PM] finished MorseCodeConverter test cases

--- src/MorseCodeConverterTest_STUDENT.java
+++ src/MorseCodeConverterTest_STUDENT.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 class MorseCodeConverterTest_STUDENT {
 
     @BeforeEach
@@ -28,31 +30,54 @@ class MorseCodeConverterTest_STUDENT {
     	String converter1 = MorseCodeConverter.convertToEnglish("-- -.-. / -.-. -- ... -.-.");
     	String converter2 = MorseCodeConverter.convertToEnglish(".... .- ...- . / .- / -. .. -.-. . / -.. .- -.--");
     	assertEquals("mc cmsc", converter1);
-    	assertEquals("have a nice day", converter2);    }
+    	assertEquals("have a nice day", converter2);    
+    }
 
     @Test
     public void testHowDoILoveTheeFile() {
-	fail("Not yet implemented");
+    	File file = new File("C:\\howDoILoveThee.txt");
+    	try {
+    		String converter = MorseCodeConverter.convertToEnglish(file);
+        	assertEquals("how do i love thee let me count the ways", converter);
+    	} catch (Exception e) {
+    	}
     }
 
     @Test
     public void testDaisyFile() {
-	fail("Not yet implemented");
-    }
+    	File file = new File("C:\\Daisy.txt");
+    	try {
+    		String converter = MorseCodeConverter.convertToEnglish(file);
+        	assertEquals("give me your answer do", converter);
+    	} catch (Exception e) {
+    	}    }
 
     @Test
     public void testDaisyDaisyFile() {
-	fail("Not yet implemented");
-    }
+    	File file = new File("C:\\DaisyDaisy.txt");
+    	try {
+    		String converter = MorseCodeConverter.convertToEnglish(file);
+        	assertEquals("im half crazy all for the love of you", converter);
+    	} catch (Exception e) {
+    	}    }
 
     @Test
     public void testLoveLooksNotFile() {
-	fail("Not yet implemented");
+    	File file = new File("C:\\LoveLooksNot.txt");
+    	try {
+    		String converter = MorseCodeConverter.convertToEnglish(file);
+        	assertEquals("love looks not with the eyes but with the mind", converter);
+    	} catch (Exception e) {
+    	}
     }
 
     @Test
     public void testStudentFiles() {
-	fail("Not yet implemented");
-    }
+    	File file = new File("C:\\StudentFile.txt");
+    	try {
+    		String converter = MorseCodeConverter.convertToEnglish(file);
+        	assertEquals("have a nice day sos hello world mc cmsc", converter);
+    	} catch (Exception e) {
+    	}    }
 
 }
